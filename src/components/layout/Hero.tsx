3b4df8ec0a14f90ee77ad166806d9ba032b0d726
Alexandre Neves <alexandre1999neves@gmail.com>
2024-03-13 19:40:27 +0000

    🥤 WIP - Hero Section

---
+++ src/components/layout/Hero.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import Right from '../icons/Right';
+
+export default function Hero() {
+    return (
+
+        <section className='hero'>
+            <div className='py-8'>
+            <h1 className="text-4xl font-semibold">
+                Everything <br/> is better with a <br/>
+                <span className="text-primary">
+                    Meal
+                </span>
+            </h1>
+            <p className="my-4 text-gray-500">Pizza is the missing piece that makes every day complete, a simple yet delicious joy in life</p>
+            <div className="flex gap-4 text-sm">
+                <button className="bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full text-sm">
+                    Order now
+                    <Right />
+                </button>
+               
+                <button className="flex gap-2 py-2 text-gray-600 font-semibold">
+                    Learn More
+                    <Right />
+                </button>
+            </div>
+            </div>
+
+            <div className="relative">
+                <Image src={'/mealprep.png'} height={'250'} width={'250'} alt={'Meal'} />
+            </div>
+        
+        </section>
+
+       
+
+    );
+}
