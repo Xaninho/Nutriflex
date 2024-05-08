34ab319d79526607ff73cd15630f8a2ea3cbd9a3
Alexandre <alexandre.neves@garland.pt>
2024-05-08 19:23:54 +0100

    🍥 WIP: Profile Image Edit

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -8,23 +8,57 @@ export default function ProfilePage() {
 
     const session = useSession();
     const [userName, setUserName] = useState('');
+    const [saved, setSaved] = useState(false);
+    const [image, setImage] = useState('');
+    const [isUploading, setIsUploading] = useState(false);
+    const [isSaving, setIsSaving] = useState(false);
     const {status} = session;
 
     useEffect(() => {
         if (status === 'authenticated') {
             setUserName(session?.data?.user?.name);
+            setImage(session?.data?.user?.image);
         }
     }, [session, status]);
 
+    async function handleFileChange(ev : any) {
+        const files = ev.target.files;
+
+        if (files?.length > 0) {
+
+            const data = new FormData();
+            data.set('file', files[0]);
+
+            setIsUploading(true);
+            const response = await fetch('/api/upload', {
+                method: 'POST',
+                body: data
+            })
+
+            const link = await response.json();
+            setImage(link);
+            setIsUploading(false);
+        }
+        
+    }
 
     async function handleProfileInfoUpdate(ev : any) {
+
         ev.preventDefault();
+        setSaved(false);
+        setIsSaving(true);
 
         const response = await fetch('/api/profile', {
             method: 'PUT',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({ name: userName})
         });
+
+        setIsSaving(false);
+
+        if (response.ok) {
+            setSaved(true);
+        }
     }
 
     if (status === 'loading') {
@@ -35,20 +69,42 @@ export default function ProfilePage() {
         redirect('/login');
     }
 
-    const userImage = session?.data?.user?.image;
-
     return (
         <section className="mt-8">
             <h1 className="text-center text-primary text-4xl mb-4">
                 Profile
             </h1>
-
+            <h2 className="text-center bg-green-100 p-4 rounded-lg border border-green-300">
+                Profile saved!
+            </h2>
             <div className="max-w-ws mx-auto">
+                {saved && (
+                    <h2 className="text-center bg-green-100 p-4 rounded-lg border border-green-300">
+                        Profile Saved!
+                    </h2>
+                )}
+                {isSaving && (
+                    <h2 className="text-center bg-blue-100 p-4 rounded-lg border border-blue-300">
+                        Saving...
+                    </h2>
+                )}
+                {isUploading && (
+                    <h2 className="text-center bg-blue-100 p-4 rounded-lg border border-blue-300">
+                        Uploading...
+                    </h2>
+                )}
                 <div className="flex gap-4 items-center">
                     <div>
-                        <div className="p2 rounded-lg relative">
-                            <Image className="rounded-lg w-full h-full mb-1" src={userImage} width={0} height={0} alt= {'avatar'}/>
-                            <button type="button"> Change avatar </button>
+                        <div className="p-2 rounded-lg relative max-w-[120px]">
+
+                            {image && (
+                                <Image className="rounded-lg w-full h-full mb-1" src={image} width={0} height={0} alt= {'avatar'}/>
+                            )}
+
+                            <label>
+                                <input type="file" className="hidden" onChange={handleFileChange} />
+                                <span className="block border border-gray-300 rounded-lg p-2 text-center">Change Avatar</span>
+                            </label>
                         </div>
                     </div>    
                     <form className="grow" onSubmit={handleProfileInfoUpdate}>
--- src/models/user.ts
+++ src/models/user.ts
@@ -2,11 +2,12 @@ import {Schema, model, models} from "mongoose";
 import bcrypt from "bcrypt";
 
 const UserSchema = new Schema ({
-    email: {type: String, required: true, unique: true },
-    password: {
-        type: String,
-        required: true,
-    }
+
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String,required: true },
+    image: { type: String, required: false }
+
 }, {timestamps: true});
 
 export const User = models?.User || model('User', UserSchema);
