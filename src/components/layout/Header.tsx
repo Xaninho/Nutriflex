deb0b9ab3d81434cb3cf1cf2ee48b5e8a292f501
João Silva <joaocsilva666@gmail.com>
2024-08-17 00:38:36 +0100

    🐞 Bug Fixes & Updates

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -56,8 +56,8 @@ export default function Header() {
     userName = userName.split(' ')[0];
   }
   return (
-    <header className="px-20 p-4">
-      <div className="flex items-center md:hidden justify-between">
+    <header className="px-20 p-4 ">
+      <div className="flex items-center md:hidden justify-between ">
         <Link className="text-primary font-semibold text-2xl" href={'/'}>
           Nutriflex
         </Link>
@@ -81,10 +81,10 @@ export default function Header() {
         <div
           onClick={() => setMobileNavOpen(false)}
           className="md:hidden p-4 bg-gray-200 rounded-lg mt-2 flex flex-col gap-2 text-center">
-          <Link href={'/'}>Home</Link>
-          <Link href={'/menu'}>Menu</Link>
-          <Link href={'/#about'}>About</Link>
-          <Link href={'/#contact'}>Contact</Link>
+          <Link className="hover:text-black transition-all font-medium" href={'/'}>Home</Link>
+          <Link className="hover:text-black transition-all font-medium" href={'/menu'}>Menu</Link>
+          <Link className="hover:text-black transition-all font-medium" href={'/#about'}>About</Link>
+          <Link className="hover:text-black transition-all font-medium" href={'/#contact'}>Contact</Link>
           <AuthLinks status={status} userName={userName} />
         </div>
       )}
@@ -93,10 +93,10 @@ export default function Header() {
           <Link className="text-primary font-semibold text-2xl" href={'/'}>
             Nutriflex
           </Link>
-          <Link href={'/'}>Home</Link>
-          <Link href={'/menu'}>Menu</Link>
-          <Link href={'/#about'}>About</Link>
-          <Link href={'/#contact'}>Contact</Link>
+          <Link className="hover:text-black transition-all font-medium" href={'/'}>Home</Link>
+          <Link className="hover:text-black transition-all font-medium" href={'/menu'}>Menu</Link>
+          <Link className="hover:text-black transition-all font-medium" href={'/#about'}>About</Link>
+          <Link className="hover:text-black transition-all font-medium" href={'/#contact'}>Contact</Link>
         </nav>
         <nav className="flex items-center gap-4 text-gray-500 font-semibold">
           <AuthLinks status={status} userName={userName} />
--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -6,15 +6,15 @@ export default function Hero() {
 
     
        // div with relative position
-        <div className="relative h-[600px] md:h-[600px] lg:h-[600px] xl:h-[600px] 2xl:h-[600px] bg-[url('/images/banner.png')] flex bg-cover px-20">
+       <div className="relative h-[700px] bg-[url('/images/banner.png')] bg-cover bg-fixed flex px-20">            
 
-            <div className="flex flex-col justify-center content-center text-white gap-3">
-                <h1 className="text-7xl font-semibold leading-15">
+            <div className="flex flex-col justify-center content-center text-white gap-3 z-20">
+                <h1 className="text-8xl font-semibold leading-15">
                     Everything <br/> is better with<br/>
                     a
                     <span className="text-primary"> Meal</span>
                 </h1>
-                <p className="text-lg mt-2">Clean-eating. Time-saving. Chef-prepared deliciousness.</p>
+                <p className="text-2xl mt-2">Clean-eating. Time-saving. Chef-prepared deliciousness.</p>
                 <div className="flex gap-4 text-sm">
                     <button className="w-[250px] bg-primary h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg
  after:absolute after:top-0 after:-left-full after:w-full after:h-full after:bg-gradient-to-r after:from-red-900 after:to-[rgba(241, 58, 1, 0.5)] after:transition-all after:duration-500 after:ease-in-out after:z-[-1] after:rounded-xl hover:after:left-0 text-[#fff]">
@@ -24,7 +24,7 @@ export default function Hero() {
                 </div>
             </div>
         
-            
+            <div className="absolute inset-0 bg-gradient-to-r from-black/80 to-transparent z-10"></div>
           
         </div>
     
